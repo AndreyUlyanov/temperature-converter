e22aba11ee24039d7fe118c0c54512e8da2225bb
Andrew Ulyanov <ulyanov2.aa@edu.spbstu.ru>
2021-11-30 17:46:47 +0300

    Adding input split function

---
+++ getting_parameters.py
@@ -0,0 +1,15 @@
+def getting_parameters(input_string):
+    scales = ('C', 'F')
+
+    parameters = input_string.split(' ')
+
+    if len(parameters) != 3:
+        raise ValueError('Input string must contain 3 parameters. Please, try again.')
+
+    if parameters[1] not in scales or parameters[2] not in scales:
+        raise ValueError('Unknown temperature scale. Please, try again.')
+
+    parameters[0] = float(parameters[0])
+
+    return parameters
+
